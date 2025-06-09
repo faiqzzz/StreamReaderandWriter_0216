66b362ee880d5802c91a47d65a0125773b9933ac
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-09 17:32:28 +0700

    menutup file nya

--- iofile.cpp
+++ iofile.cpp
@@ -26,5 +26,6 @@ int main()
         // menulis dan memasukkan nilai dari 'baris' ke dalam file 
         outfile << baris << endl;
     }
-
+// selesai dalam menulis sekarang tutup file nya
+outfile.close();
 }
