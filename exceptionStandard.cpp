0aba3480b0a2619c18d45707f516448e533348f5
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-09 19:01:51 +0700

    program utama exception standard

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -4,3 +4,24 @@
 #include <array>
 // untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main ()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array interger 3 elemen
+        cout << data.at(4) << endl;
+        // memanggil array elemen ke 5
+    }
+    catch(exception& e)
+    {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris program yang terakhir" << endl;
+    /* Penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalaahn*/
+    return 0;
+}
