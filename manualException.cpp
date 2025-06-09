08ff12a2dd17b5c37717482ea9e2033a13acfc13
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-09 18:51:43 +0700

    melemparkan sebuah interger maka

--- manualException.cpp
+++ manualException.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw("halo"); // melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan di eksekusi" << endl;
+    }
+ 
+
+}
