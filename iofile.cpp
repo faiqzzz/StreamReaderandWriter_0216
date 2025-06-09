1a06c16630dd63d4c966e044aa5ba884c49ed9ff
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-09 17:32:09 +0700

    menulis dan memasukkan nilai dari 'baris' ke dalam file

--- iofile.cpp
+++ iofile.cpp
@@ -23,5 +23,8 @@ int main()
         // Loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q")
             break;
-        
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file 
+        outfile << baris << endl;
+    }
+
 }
