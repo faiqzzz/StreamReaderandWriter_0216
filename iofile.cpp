e648f9fb082649800bd955b26371aa1cd3ea3168
faiqzzz <ridhofaiqzzz@gmail.com>
2025-06-09 17:31:23 +0700

    mendapatkan setiap karakter dalam satu baris

--- iofile.cpp
+++ iofile.cpp
@@ -17,7 +17,8 @@ int main()
     // unlimited loop untuk menulis
     while (true)
     {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
         
-    }
-
 }
